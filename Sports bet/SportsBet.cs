165ea55aa0aeda3442dab18cd96d1a8ccc949244
damsii <d.miloshevski@gmail.com>
2015-03-31 20:45:20 +0200

    Sports bet application

--- Sports bet/SportsBet.cs
+++ Sports bet/SportsBet.cs
@@ -8,10 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SportsBet
-{
-    public partial class SportsBet : Form
-    { /*
+namespace SportsBet1_1
+{     /*
         ** author: Damjan Miloshevski
         ** contact: d.miloshevski@gmail.com;
         ** skype: damjan.milosevski
@@ -19,200 +17,202 @@ namespace SportsBet
         ** web: https://www.facebook.com/damjan.miloshevski
                 http://miloshevski.us.to/
       */
+    public partial class SportsBet : Form
+    {//v.1.1
         public SportsBet()
         {
             InitializeComponent();
 
-            Team t = new Team();
-            t.Name = "Манчестер Јунајтед";
-            t.Country = "Англија";
-            lstTeams.Items.Add(t);
+            Team a = new Team("Manchester United", "England");
+            Team b = new Team("Arsenal", "England");
+            Team c = new Team("Real Madrid", "Spain");
+            Team d = new Team("Barcelona", "Spain");
 
-            Team t1 = new Team();
-            t1.Name = "Барселона";
-            t1.Country = "Шпанија";
-            lstTeams.Items.Add(t1);
-
-            Team t2 = new Team();
-            t2.Name = "Јувентус";
-            t2.Country = "Италија";
-            lstTeams.Items.Add(t2);
-
-            Team t3 = new Team();
-            t3.Name = "Челзи";
-            t3.Country = "Англија";
-            lstTeams.Items.Add(t3);
-
-            Team t4 = new Team();
-            t4.Name = "Манчестер Сити";
-            t4.Country = "Англија";
-            lstTeams.Items.Add(t4);
-
-            Team t5 = new Team();
-            t5.Name = "Баерн Минхен";
-            t5.Country = "Германија";
-            lstTeams.Items.Add(t5);
-
-            Team t6 = new Team();
-            t6.Name = "Шалке 04";
-            t6.Country = "Германија";
-            lstTeams.Items.Add(t6);
-
-            Team t7 = new Team();
-            t7.Name = "Реал Мадрид";
-            t7.Country = "Шпанија";
-            lstTeams.Items.Add(t7);
-
-            Team t8 = new Team();
-            t8.Name = "Париз Сен Жермен";
-            t8.Country = "Франција";
-            lstTeams.Items.Add(t8);
-
-            Team t9 = new Team();
-            t9.Name = "Милан";
-            t9.Country = "Италија";
-            lstTeams.Items.Add(t9);
-        }
-
-        private void btnAddTeam_Click(object sender, EventArgs e)
-        {
-            NewTeam nt = new NewTeam();
-            if (nt.ShowDialog() == DialogResult.OK)
-            {
-                lstTeams.Items.Add(nt.Team);
-            }
-        }
-
-        private void mtbCode_Validating(object sender, CancelEventArgs e)
-        {
-            if (!mtbCode.MaskCompleted)
-            {
-                e.Cancel = true;
-                errorCode.SetError(mtbCode, "Внесете четири цифри!");
-            }
-            else
-            {
-                errorCode.SetError(mtbCode, null);
-            }
+            lstTeams.Items.Add(a);
+            lstTeams.Items.Add(b);
+            lstTeams.Items.Add(c);
+            lstTeams.Items.Add(d);
         }
 
         private void btnAddToBilten_Click(object sender, EventArgs e)
         {
-            if (!ValidateChildren())
-            {//gi proveruva site elementi vo formata i vrakja false ako ne se kompletirani, vo sprotivno true
-                return;
-            }
+            if (!ValidateChildren()) return;//gi proveruva site elementi vo formata
             if (lstTeams.SelectedItems.Count == 2)
             {
-                Game g = new Game();
-                g.Code = mtbCode.Text;
-                g.Home = lstTeams.SelectedItems[0] as Team;
-                g.Guest = lstTeams.SelectedItems[1] as Team;
-                g.Coefitients[0] = (float)numKoef1.Value;
-                g.Coefitients[1] = (float)numKoefX.Value;
-                g.Coefitients[2] = (float)numKoef2.Value;
+                Team a = lstTeams.SelectedItems[0] as Team;
+                Team b = lstTeams.SelectedItems[1] as Team;
+                Game g = new Game(a, b, mbtbCode.Text);
+                g.Coefitients[0] = (float)numTip1.Value;
+                g.Coefitients[1] = (float)numTipX.Value;
+                g.Coefitients[2] = (float)numTip2.Value;
 
-                if (!sameCountry())
+                if (sameCountry())
                 {
-                    MessageBox.Show("Изберете два тима од иста држава!", "Грешка!", MessageBoxButtons.OK,
-                   MessageBoxIcon.Error);
-                }
-                else
-                {
-                    if (containsTeam(g))
+                    if (!sameCode(g))
                     {
-                        MessageBox.Show("Тој натпревар е веќе внесен!", "Грешка!", MessageBoxButtons.OK,
-                           MessageBoxIcon.Error);
+                        lstBilten.Items.Add(g);
+                        lstTeams.ClearSelected();
+                        mbtbCode.Clear();
+                        numTip1.ResetText();
+                        numTipX.ResetText();
+                        numTip2.ResetText();
                     }
                     else
                     {
-                        lstGames.Items.Add(g);
-                        numKoef1.Value = 1;
-                        numKoefX.Value = 1;
-                        numKoef2.Value = 1;
-                        lstTeams.ClearSelected();
-                        mtbCode.Clear();
+                        DialogResult response = MessageBox.Show("Овој натпревар е веќе внесен!", "Грешка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        if (response == DialogResult.OK)
+                        {
+                            mbtbCode.Clear();
+                            mbtbCode.Focus();
+                            lstTeams.ClearSelected();
+                        }
                     }
+                }
+                else
+                {
+                    MessageBox.Show("Изберете два тима од иста држава!", "Грешка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    lstTeams.ClearSelected();
                 }
             }
             else
             {
-                MessageBox.Show("Изберете точно два тимa!", "Грешка!", MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
+                MessageBox.Show("Изберете 2 тима!", "Грешка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lstTeams.ClearSelected();
             }
         }
-        bool containsTeam(Game g)
+        private bool sameCode(Game c)
         {
-            foreach (object obj in lstGames.Items)
+            foreach (object obj in lstBilten.Items)
             {
-                Game game = obj as Game;
-                if (game.Code.Equals(g.Code))
-                {
-                    return true;
-                }
+                Game g = obj as Game;
+                if (g.Code.Equals(c.Code)) return true;
             }
             return false;
         }
-        bool containsTicketItem(TicketItem ti)
-        {
-            foreach (object obj in lstTiket.Items)
-            {
-                TicketItem tItem = obj as TicketItem;
-                if (ti.Game.Code.Equals(tItem.Game.Code))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-        bool sameCountry()
+        private bool sameCountry()
         {
             Team a = lstTeams.SelectedItems[0] as Team;
             Team b = lstTeams.SelectedItems[1] as Team;
             return a.Country.Equals(b.Country);
         }
+        private bool containsTeam(Team a)
+        {
+            foreach (object obj in lstTeams.Items)
+            {
+                Team b = obj as Team;
+                if (a.Name.Equals(b.Name)) return true;
+            }
+            return false;
+        }
+        private void mbtbCode_Validating(object sender, CancelEventArgs e)
+        {
+            if (!mbtbCode.MaskCompleted)
+            {
+                e.Cancel = true;
+                errorCode.SetError(mbtbCode, "Шифрата треба има 4 цифри!");
+            }
+            else errorCode.SetError(mbtbCode, null);
+        }
+        private void calculateTotal()
+        {
+            float totalCoeficient = 1;
+            float dobivka = 1;
+            foreach (object obj in lstTicket.Items)
+            {
+                TicketItem t = obj as TicketItem;
+                totalCoeficient *= t.Game.Coefitients[t.Tip];//ja zema vrednosta na koeficientot od game i ja stava vo nizata na koeficienti
+            }
+            dobivka = totalCoeficient * (int)numUplata.Value;
+            tbVkupnoKoef.Text = string.Format("{0:0.0000}", totalCoeficient.ToString());
+            tbDobivka.Text = string.Format("{0:0.000} ден.", dobivka.ToString());
+        }
+        private void btnAddNewTeam_Click(object sender, EventArgs e)
+        {
+            NewTeam nt = new NewTeam();
+            DialogResult response = nt.ShowDialog();
+            if (response == DialogResult.OK)
+            {
+                if (!containsTeam(nt.Team))
+                    lstTeams.Items.Add(nt.Team);
+                else
+                    MessageBox.Show("Тој тим веќе постои!", "Грешка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
         private void btnAddGame_Click(object sender, EventArgs e)
         {
-            if (lstGames.SelectedIndex != -1 && cbTip.SelectedIndex != -1)
+            if (lstBilten.SelectedIndex != -1)
             {
-                Game g = lstGames.SelectedItem as Game;
-                TicketItem ti = new TicketItem();
-                ti.Game = g;
-                ti.Tip = cbTip.SelectedIndex;
-                if (!containsTicketItem(ti))
+                if (cbTip.SelectedIndex != -1)
                 {
-                    lstTiket.Items.Add(ti);
-                    lstGames.ClearSelected();
-                    calculateTotal();
-                    cbTip.SelectedIndex = 0;
+                    Game g = lstBilten.SelectedItem as Game;
+                    TicketItem ti = new TicketItem();
+                    ti.Game = g;
+                    ti.Tip = cbTip.SelectedIndex;
+                    if (!containsItem(ti))
+                    {
+                        lstTicket.Items.Add(ti);
+                        cbTip.SelectedIndex = -1;
+                        lstBilten.ClearSelected();
+                        calculateTotal();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Овој натпревар е веќе типуван!", "Грешка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        lstBilten.ClearSelected();
+                    }
                 }
                 else
-                {
-                    MessageBox.Show("Тој натпревар е веќе типуван!", "Грешка!", MessageBoxButtons.OK,
-                       MessageBoxIcon.Error);
-                    return;
-                }
+                    MessageBox.Show("Изберете тип!", "Информација!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cbTip.Focus();
             }
             else
             {
-                MessageBox.Show("Немате селектирано натпревар или тип!", "Грешка!", MessageBoxButtons.OK,
-                      MessageBoxIcon.Error);
-                return;
+                MessageBox.Show("Немате избрано натпревар!", "Грешка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        public void calculateTotal()
+        private bool containsItem(TicketItem t)
         {
-            float totalCoeficient = 1;
-            foreach (object obj in lstTiket.Items)
+            foreach (object obj in lstTicket.Items)
             {
-                TicketItem ti = obj as TicketItem;
-                totalCoeficient *= ti.Game.Coefitients[ti.Tip];
+                TicketItem i = obj as TicketItem;
+                if (i.Game.Code.Equals(t.Game.Code)) return true;
             }
-            tbTotalKoef.Text = string.Format("{0:0.000}", totalCoeficient.ToString());
-            float profit = totalCoeficient * (float)numUplata.Value;
-            tbDobivka.Text = string.Format("{0:0.0}", profit);
+            return false;
         }
-
+        private void tbCode_TextChanged(object sender, EventArgs e)
+        {
+            if (lstBilten.Items.Count > 0)
+            {
+                foreach (object obj in lstBilten.Items)
+                {
+                    Game g = obj as Game;
+                    TicketItem item = new TicketItem();
+                    if (cbTip.SelectedIndex != -1)
+                    {
+                        if (g.Code.Equals(tbCode.Text))
+                        {
+                            item.Game = g;
+                            if (!containsItem(item))
+                            {
+                                lstTicket.Items.Add(item);
+                                calculateTotal();
+                                tbCode.Text = null;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Овој натпревар е веќе типуван!", "Грешка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Изберете тип!", "Информација!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        cbTip.Focus();
+                    }
+                }
+            }
+        }
         private void numUplata_ValueChanged(object sender, EventArgs e)
         {
             calculateTotal();
@@ -220,45 +220,15 @@ namespace SportsBet
 
         private void btnPrint_Click(object sender, EventArgs e)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.Append(string.Format("За вложени {0:0.0} добивката е {1}",
-                numUplata.Value, tbDobivka.Text));
-            MessageBox.Show(sb.ToString(), "Информација", MessageBoxButtons.OK,
-                MessageBoxIcon.Information);
-        }
-
-        private void txtCode_TextChanged(object sender, EventArgs e)
-        {
-            if (cbTip.SelectedIndex != -1)
+            if (lstTicket.Items.Count > 0)
             {
-                foreach (object obj in lstGames.Items)
-                {
-                    Game g = obj as Game;
-                    TicketItem ti = new TicketItem();
-                    if (txtCode.Text.Equals(Convert.ToString(g.Code)))
-                    {
-                        ti.Game = g;
-                        ti.Tip = cbTip.SelectedIndex;
-                        if (!containsTicketItem(ti))
-                        {
-                            lstTiket.Items.Add(ti);
-                            calculateTotal();
-                            txtCode.Text = null;
-                            cbTip.SelectedIndex = 0;
-                        }
-                        else
-                        {
-                            MessageBox.Show("Тој натпревар е веќе типуван!", "Грешка!", MessageBoxButtons.OK,
-                              MessageBoxIcon.Error);
-                            return;
-                        }
-                    }
-                }
+                StringBuilder sb = new StringBuilder();
+                sb.Append("За уплатени " + (int)numUplata.Value + " ден. , добивката е " + tbDobivka.Text.ToString());
+                MessageBox.Show(sb.ToString(), "Вашата уплата", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("Изберете тип!", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
+                MessageBox.Show("Тикетот е празен! :(", "Вашата уплата", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
     }
